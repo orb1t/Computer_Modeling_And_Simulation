459d87b9d797e63c2c7c665e9ccd2b60c1f3fc2b
DESKTOP-1OUEFC4\Efode <efidesalew@gmail.com>
2018-02-04 09:02:48 -0800

    Update

--- Simulation1/Simulation1/Simulation1.cpp
+++ Simulation1/Simulation1/Simulation1.cpp
@@ -1,35 +1,41 @@
-// Simulation1.cpp : Defines the entry point for the console application.
-//
-
-#include "stdafx.h"
 #include <iostream>
-#include <chrono>
 using namespace std;
-
-int _tmain(int argc, _TCHAR* argv[])
+int main(void)
 {
-	int x = 0;
-	int arrivalTime = 0;//A variable to hold the user's input(Inter Araival Time)
-	int serviceTime = 0;//A variable to hold the user's input(Service Time)
-	int processRows = 2;//A vribale to hold 
+	int simulate = 0;
+	float arrivalTime = 0;
+	float serviceTime = 0;
+	int processRows = 2;
 	int processColumns = 2;
 	int simulationRows = 0;
 	int simulationColumns = 5;
-	cout << "Enter number of Processes"<<endl;
+	float simulationTime = 0;
+	float* arrivalTimes = 0;
+	float* beginTimes = 0;
+	float* endTimes = 0;
+	float* waitingTimes = 0;
+	float* systemTime = 0;
+	float averageSyetemTime = 0;
+	cout << "Enter number of Processes" << endl;
 	cin >> processRows;
 	simulationRows = processRows;
-	int **processes = 0;
-	int **simulation = 0;
-	processes = new int *[processRows];
-	simulation = new int *[simulationRows];
+	float **processes = 0;
+	float **simulation = 0;
+	processes = new float *[processRows];
+	simulation = new float *[simulationRows];
+	arrivalTimes = new float[processRows];
+	beginTimes = new float[processRows];
+	endTimes = new float[processRows];
+	waitingTimes = new float[processRows];
+	systemTime = new float[processRows];
 
 	for (int i = 0; i < processRows; i++)
 	{
-		processes[i] = new int[processColumns];
+		processes[i] = new float[processColumns];
 	}
 	for (int i = 0; i < simulationRows; i++)
 	{
-		simulation[i] = new int[simulationColumns];
+		simulation[i] = new float[simulationColumns];
 	}
 
 	for (int i = 0; i < processRows; i++)
@@ -51,21 +57,158 @@ int _tmain(int argc, _TCHAR* argv[])
 			
 		}
 	}
+	cout << "#############################################" << endl;
+	cout << "You Have Entered The Following Process Data" << endl;
+	cout << "#############################################"<< endl;
+	cout << "# NO  # Inter Arrival Time  # Service Time  #"<< endl;
+	for (int i = 0; i < processRows; i++)
+	{
+		cout << "#";
+		cout << " ";
+		cout <<i+1;
+		cout << "   #";
+		for (int j = 0; j < processColumns; j++)
+		{
+			
+			if (j == 0)
+			{
+				cout << " ";
+				cout << processes[i][j];
+				cout << "                   #";
+			}
+			else
+			{
+				cout << " ";
+				cout << processes[i][j];
+				cout << "             #";
+			}
+		}
+		cout << endl;
+		cout << "#############################################"<<endl;
+	}
 
 	for (int i = 0; i < processRows; i++)
 	{
-		for (int j = 0; j < processColumns; j++)
+		if (i == 0)
 		{
-			cout << "| ";
-			cout<<processes[i][j];
-			cout << " |";
+			arrivalTimes[i] = processes[i][0];
 		}
-		cout << endl;
+		else
+		{
+			arrivalTimes[i] = arrivalTimes[i - 1] + processes[i][0];
+		}
+	}
+	for (int i = 0; i < processRows; i++)
+	{
+		if (i == 0)
+		{
+			beginTimes[i] = arrivalTimes[i];
+		}
+		else
+		{
+			if (arrivalTimes[i] <= (beginTimes[i - 1] + processes[i - 1][1]))
+			{
+				beginTimes[i] = beginTimes[i-1] + processes[i - 1][1];
+			}
+			else
+			{
+				
+				beginTimes[i] = arrivalTimes[i];
+			}
+		}
+	}
+	for (int i = 0; i < processRows; i++)
+	{
+		endTimes[i] = beginTimes[i] + processes[i][1];
+	}
+	for (int i = 0; i < processRows; i++)
+	{
+		if (i == 0)
+		{
+			waitingTimes[i] = 0;
+		}
+		else
+		{
+			waitingTimes[i] = beginTimes[i] - arrivalTimes[i];
+		}
+		
+	}
+	for (int i = 0; i < processRows;i++)
+	{
+		systemTime[i] = endTimes[i] - arrivalTimes[i];
+	}
+	cout << "If you want to simulate press 1"<<endl;
+	cin >> simulate;
+	if (simulate == 1)
+	{
+		cout << "Please Enter Simulation Time"<<endl;
+		cin >> simulationTime;
+		cout << "Simulation Output" << endl;
+		//Simulation table output formatting
+		cout << "################################################################################" << endl;
+		cout << "# No  #" << " Arrival Time  #" << " Begin Time  #" << " End Time  #" << " System Time  #" << " Waiting Time  #" << endl;
+		for (int i = 0; i < processRows; i++)
+		{
+			cout << "# ";
+			cout << i + 1 << " ";
+			cout << "  # ";
+			cout << arrivalTimes[i];
+			cout << "             # ";
+			cout << beginTimes[i];
+			cout << "           # ";
+			cout << endTimes[i];
+			cout << "         # ";
+			cout << systemTime[i];
+			cout << "            # ";
+			cout << waitingTimes[i];
+			cout << "             #";
+			cout << endl;
+		}
+		cout << "################################################################################" << endl;
+	}
+	int compeletedTasks = 0;
+	float completedTasksTotalTime = 0;
+	float completedTasksTotalWaitingTime = 0;
+	float totalUtlization = 0;
+	for (int i = 0; i < processRows; i++)
+	{
+		if (endTimes[i] <= simulationTime)
+		{
+			compeletedTasks++;
+		}
 	}
 
+	for (int i = 0; i < compeletedTasks; i++)
+	{
+		completedTasksTotalTime += systemTime[i];
+	}
+	cout << "Average System Time:"<<completedTasksTotalTime/(compeletedTasks)<<endl;
+
+	for (int i = 0; i < compeletedTasks; i++)
+	{
+		completedTasksTotalWaitingTime += waitingTimes[i];
+	}
+	cout << "Avarage Queue Time:"<<completedTasksTotalWaitingTime/compeletedTasks<<endl;
 	
-	
-	cin >> x;
+	for (int i = 0; i < compeletedTasks;i++)
+	{
+		int residue = 0;
+		if (i == compeletedTasks - 1)
+		{
+			if (endTimes[i] < simulationTime)
+			{
+				residue = simulationTime - endTimes[i];
+				totalUtlization += (endTimes[i] - beginTimes[i])+residue;
+			}
+		}
+		else
+		{
+			totalUtlization += endTimes[i] - beginTimes[i];
+		}
+		
+	}
+	cout << "Resource Utlization:"<<totalUtlization/simulationTime<<endl;
+	cin >> simulate;
 	return 0;
 }
 
